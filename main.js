fcaefdde3e62a7bb0b3ab45180e7e9084cef2659
anushkasahoo <85781997+anushkasahoo@users.noreply.github.com>
2021-08-20 15:51:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function Classify() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/USi_wgHV9/model.json',modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResult);
+}
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+    }
+}
